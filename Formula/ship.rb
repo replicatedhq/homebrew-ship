951e8b8a0a78d7c228135a5a90fc6afff916a4f9
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-17 16:54:14 -0800

    Brew formula update for ship version v0.29.0

--- Formula/ship.rb
+++ Formula/ship.rb
@@ -1,9 +1,9 @@
 class Ship < Formula
   desc "Deploy 3rd party applications through modern pipelines"
   homepage "https://ship.replicated.com/"
-  url "https://github.com/replicatedhq/ship/releases/download/v0.28.0/ship_0.28.0_darwin_amd64.tar.gz"
-  version "0.28.0"
-  sha256 "02c70cc4575a3011b92c442cfb5e1106644bd9eac009ed8bb08002fbb3bae70d"
+  url "https://github.com/replicatedhq/ship/releases/download/v0.29.0/ship_0.29.0_darwin_amd64.tar.gz"
+  version "0.29.0"
+  sha256 "80439b4f2604230b512ad00b2a2a72967511edc076ec0b841230ce90828f9c11"
 
   def install
     bin.install "ship"
